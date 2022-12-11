fc413a50a808e3d8f8604383c05a12298ab5f405
Maria Riosdelgado <mariariosdelgado@Marias-iMac.local>
2022-12-11 10:22:31 -0500

    updated flashcards

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import flashcardList from './components/flashcard_list';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   const [cards, setCards] = useState(SAMPLE_CARDS)
+
+   return (
+     <flashcardList flashcards={cards} />
   );
 }
+
+const SAMPLE_CARDS = [
+  {
+    id: 1,
+    question: 'What is 2 + 2?',
+    answer: '4',
+    options: ['2', '3', '5', '4']
+  },
+  {
+    id: 2,
+    question: 'What is three + three?',
+    answer: '6',
+    options: ['2', '6', '8', '4']
+  },
+];
 
 export default App;
