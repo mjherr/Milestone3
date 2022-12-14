2f258e12bcf949774264133bb0c50ed9f2bd487d
Maria Riosdelgado <mariariosdelgado@Marias-iMac.local>
2022-12-14 10:28:17 -0500

    updated server

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,21 @@
-const mongoose = require('mongoose')
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "../../Backend/routes/user_routes";
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) })
+const app = express();
+dotenv.config();
+
+
+//middleware
+app.use("/user", userRouter);
+
+//connections
+mongoose.connect("MONGO_URI=mongodb://localhost:27017/travel", {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+app.use("/", (req, res) => {
+  res.send('home');
+})
+
+app.listen(5000, () => console.log("Listening to port 5000") )
