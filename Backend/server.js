e0f1ae631799052ed94b679650f8014bfb1e6354
Maria Riosdelgado <mariariosdelgado@Marias-iMac.local>
2022-12-16 17:25:19 -0500

    added authorization controller

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,41 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "../../Backend/routes/user_routes";
 
-const app = express();
-dotenv.config();
+// DEPENDENCIES
+const express = require('express')
+const mongoose = require('mongoose')
+// const methodOverride = require('method-override')
 
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+console.log(PORT)
+const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
 
-//middleware
-app.use("/user", userRouter);
+// MIDDLEWARE
+// app.set('views', __dirname + '/views')
+// app.set('view engine', 'jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+// app.use(express.static('public'))
+// app.use(express.urlencoded({extended: true}))
+// app.use(methodOverride('_method'))
 
-//connections
-mongoose.connect("MONGO_URI=mongodb://localhost:27017/travel", {useNewUrlParser: true, useUnifiedTopology: true})
-
-
-app.use("/", (req, res) => {
-  res.send('home');
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('¡Hola! Welcome to a place where you can post about your travels!')
 })
 
-app.listen(5000, () => console.log("Listening to port 5000") )
+//404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
+  
+// User controller
+const { default: userRouter } = require('./routes/user_routes')
+app.use('/user', userRouter)
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "../../Backend/routes/user_routes";
 
 const app = express();
 dotenv.config();
 
 
 //middleware
+app.use("/user", userRouter);
 
 //connections
-mongoose.connect("MONGO_URI=mongodb://localhost:27017/travel", {useNewUrlParser: true, useUnifiedTo})
+mongoose.connect("MONGO_URI=mongodb://localhost:27017/travel", {useNewUrlParser: true, useUnifiedTopology: true})
 
 
 app.use("/", (req, res) => {
   res.send('home');
 })
 
-app.listen(3000, () => console.log("Listening to port 3000") )
+app.listen(5000, () => console.log("Listening to port 5000") )
